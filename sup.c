614671751fe73d459bcf8e5479bf4e916a5f1f30
JosephWhetten <joewhetten21408@gmail.com>
2025-04-01 14:50:13 +0000

    Testing with If's

--- sup.c
+++ sup.c
@@ -7,5 +7,12 @@ int main() {
 
     printf("What is your age?\n");
     printf("My age is %d\n", age);
+
+    if(age <= 15) {
+       printf("You cannot play Shell Shockers\n"); 
+    }else{
+        printf("You can play Shell Shockers\n");
+    }
+
     return 0;
 }
