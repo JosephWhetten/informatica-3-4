5a713ef93b60d002ff48acd3e1d43cc80a00614d
JosephWhetten <joewhetten21408@gmail.com>
2025-04-09 16:13:16 +0000

    starting with comparison-operators

--- sup.c
+++ sup.c
@@ -3,12 +3,13 @@
 // Prints 
 int main() {
 
-    int age = 16;
+    int age;
 
     printf("What is your age?\n");
+    scanf("%d",&age);
     printf("My age is %d\n", age);
 
-    if(age <= 15) {
+    if(age <= 12) {
        printf("You cannot play Shell Shockers\n"); 
     }else{
         printf("You can play Shell Shockers\n");
--- variables-operations.c
+++ variables-operations.c
@@ -2,29 +2,32 @@
 
 int main() {
     //Rectangle
-    int l = 10,w = 15;
+    int l,w;
     int area, peri;
+    printf("Insert a length and wide of a rectangle\n");
+    scanf("%d%d",&l,&w);
     area = l*w;
-    peri = (l*2)+(w*2);
-
+    peri = (l*2)+(w*2); 
     printf("Length = %d, Width = %d\n",l,w);
     printf("Area: %d\n",area);
     printf("Perimeter: %d\n\n",peri);
 
 
     //Temperature calculator
-    int F = 98;
+    int F;
+    printf("Enter a temperature in Farenheit.\n");
+    scanf("%d",&F);
     int C = (F - 32)/1.8;
-
     printf("Temperature = %d°F\n",F);
     printf("%d°F, %d°C\n\n",F,C);
 
 
     //Minutes into hours and minutes
-    int mins = 349;
+    int mins;
+    printf("Enter a number of minutes.\n");
+    scanf("%d",&mins);
     int hours = (mins/60);
     int leftmins = (mins%60);
-
     printf("%d minutes is equal to %d hours and %d minutes.\n",mins,hours,leftmins);
 
     return 0;
