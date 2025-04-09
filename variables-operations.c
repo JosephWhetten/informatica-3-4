a047c799888668349865422bbcc2a4c28255ef73
JosephWhetten <joewhetten21408@gmail.com>
2025-04-09 16:22:55 +0000

    finished variables-operations

--- variables-operations.c
+++ variables-operations.c
@@ -12,7 +12,6 @@ int main() {
     printf("Area: %d\n",area);
     printf("Perimeter: %d\n\n",peri);
 
-
     //Temperature calculator
     int F;
     printf("Enter a temperature in Farenheit.\n");
@@ -21,14 +20,20 @@ int main() {
     printf("Temperature = %d°F\n",F);
     printf("%d°F, %d°C\n\n",F,C);
 
-
     //Minutes into hours and minutes
     int mins;
     printf("Enter a number of minutes.\n");
     scanf("%d",&mins);
     int hours = (mins/60);
     int leftmins = (mins%60);
-    printf("%d minutes is equal to %d hours and %d minutes.\n",mins,hours,leftmins);
+    printf("%d minutes is equal to %d hours and %d minutes.\n\n",mins,hours,leftmins);
+
+    //Area of a circle
+    int radius;
+    printf("Enter a radius for a circle.\n");
+    scanf("%d",&radius);
+    int areacircle = 3.14159265359*(radius^2);
+    printf("The area of a circle with a radius of %d is %d.\n",radius,areacircle);
 
     return 0;
 }
