1601cba4ec1f88ccd4a8eb67d4d24e550c08abe4
JosephWhetten <joewhetten21408@gmail.com>
2025-04-28 16:30:37 +0000

    figuring out how to do the leap year thing

---
+++ if-challenge.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    //2020 is a leap year, detect what year is a leap year
+    int year, leapyear;
+    
+/*- If the year is divisible by 4, it’s a leap year; if not, it is not a leap year.
+- If divisible by 4, check if it is divisible by 100; if not, it is a leap year.
+- Finally, if the year is divisible by both 4 and 100, check if it is divisible by 400; if yes, it is a leap year; otherwise, it is not.*/
+
+    printf("Enter a year and I'll tell you if it's a leap year or not.\n");
+    scanf("%d",&year);
+    if(year&4==0) {
+        printf("sup\n");
+
+        if(year&100!=0) {
+        printf("leapyear\n");
+        }
+    
+    }    
+    if(year/4==0 && year&100==0) {
+        if(year&400 == 0) {
+            printf("IT'S A FLIPPIN LEAP YEAR\n");
+        }
+    }
+
+    return 0;
+}
