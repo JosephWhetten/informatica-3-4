24be56701a640d6fa68bff1c63ba54209ef78174
JosephWhetten <joewhetten21408@gmail.com>
2025-05-09 14:39:31 +0000

    finished exponents with pow()

--- exponents.c
+++ exponents.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 
@@ -9,7 +10,7 @@ int main() {
     scanf("%lli",&num);
     printf("Enter an exponent\n");
     scanf("%lli",&exp);
-    printf("What loop do you want to use?\n1 - while loop\n2 - for loop\nEnter your loop:\n");
+    printf("What loop do you want to use?\n1 - while loop\n2 - for loop\n3 - pow() funcion\nEnter your loop:\n");
     scanf("%d",&loop);
     
     long long expnum = num;
@@ -20,11 +21,15 @@ int main() {
     }
     printf("%lli\n",num);
     } 
-    else {
+    else if(loop == 2) {
     for(exp; exp > 1; --exp) {
         num = num * expnum;
     }
     printf("%lli\n",num);
+    }
+    else {
+    long long power = pow(num, exp);
+    printf("%lli\n",power);
     }
 
     return 0;
