77086a61d476910659f097f240a232ed85cd222a
JosephWhetten <joewhetten21408@gmail.com>
2025-05-19 16:24:38 +0000

    finished functions

--- functions.c
+++ functions.c
@@ -12,18 +12,40 @@ int main() {
     const float URANUS = 84.016846;
     const float NEPTUNE = 164.79132;
 
-    int birth;
+    int user_age;
     int planet;
     printf("Enter your age\n");
-    scanf("%d",&birth);
-    printf("Enter the planet you want to be on:\n1. Mercury\n2. Venus\n3. Earth\n4. Mars\n5. Jupiter\n6. Saturn\n7. Uranus\n8. Neptune");
+    scanf("%d",&user_age);
+    printf("Enter the planet you want to be on:\n1. Mercury\n2. Venus\n3. Earth\n4. Mars\n5. Jupiter\n6. Saturn\n7. Uranus\n8. Neptune\n");
     scanf("%d",&planet);
 
-    for(int i = 1; i < (planet + 1); ++i) {
-        int i;
+    float answer;
+    if(planet == 1) {
+        answer = calcAge(user_age,MERCURY);
+        printf("You would be %.2f years old\n",answer);
+    } else if(planet == 2) {
+        answer = calcAge(user_age,VENUS);
+        printf("You would be %.2f years old\n",answer);
+    } else if(planet == 3) {
+        answer = calcAge(user_age,EARTH);
+        printf("Pues no duh %.2f is your age\n",answer);
+    } else if(planet == 4) {
+        answer = calcAge(user_age,MARS);
+        printf("You would be %.2f years old\n",answer);
+    } else if(planet == 5) {
+        answer = calcAge(user_age,JUPITER);
+        printf("You would be %.2f years old\n",answer);
+    } else if(planet == 6) {
+        answer = calcAge(user_age,SATURN);
+        printf("You would be %.2f years old\n",answer);
+    } else if(planet == 7) {
+        answer = calcAge(user_age,URANUS);
+        printf("You would be %.2f years old\n",answer);
+    } else if(planet == 8) {
+        answer = calcAge(user_age,NEPTUNE);
+        printf("You would be %.2f years old\n",answer);
     }
-    
-    
+
     return 0;
 }
 
